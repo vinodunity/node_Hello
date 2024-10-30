cc3c7d9e998bf7ff2063054bf3e63fe0dd184b3f
Vinod Kumar <vinod@unityhealth.net>
2024-10-30 12:36:54 +0530

    main content changed

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Welcome to Unity PHM Azure Managed App! :-)");
+    return res.send("Welcome to Unity PHM Azure Managed Application! :-)");
 })
 
 app.get('/api', function (req, res) {
